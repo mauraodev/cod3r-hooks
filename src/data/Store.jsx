4a00a99c3c06b7690ea685acef867716453f53e0
Mauro Nunes <maurao.dev@gmail.com>
2021-06-07 22:34:21 -0300

    final context 2

--- src/data/Store.jsx
+++ src/data/Store.jsx
@@ -2,16 +2,25 @@ import React, { useState } from 'react'
 
 const initialState = {
     number: 1234,
-    text: 'Context APi + Hooks'
+    text: 'Context API + Hooks'
 }
 
-const AppContext = React.createContext(initialState)
+export const AppContext = React.createContext(initialState)
 
 const Store = props => {
     const [state, setState] = useState(initialState)
-    
+
+    function updateState(key, value) {
+        setState({ ...state, [key]: value })
+    }
+
     return (
-        <AppContext.Provider>
+        <AppContext.Provider value={{
+            number: state.number,
+            text: state.text,
+            setNumber: n => updateState('number', n),
+            setText: t => updateState('text', t)
+        }}>
             {props.children}
         </AppContext.Provider>
     )
--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,7 +1,9 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 import DataContext from '../../data/DataContext'
+import { AppContext } from '../../data/Store'
 
 const UseContext = (props) => {
   const context = useContext(DataContext)
@@ -13,6 +15,14 @@ const UseContext = (props) => {
     })
   }
 
+  const { number, setNumber, text, setText } = useContext(AppContext)
+
+  useEffect(function() {
+    if (number > 1250) {
+      setText('Eita')
+    }
+  }, [number, setText])
+
   return (
     <div className="UseContext">
       <PageTitle
@@ -20,13 +30,27 @@ const UseContext = (props) => {
         subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
       />
 
+      <SectionTitle title="Exercício #1" />
+
       <div className="center">
-        <span className="text">{ context.state.text }</span>
-        <span className="text">{ context.state.number }</span>
+        <span className="text">{context.state.text}</span>
+        <span className="text">{context.state.number}</span>
 
         <div>
           <button className="btn" onClick={() => addNumber(+1)}>+1</button>
           <button className="btn" onClick={() => addNumber(-1)}>-1</button>
+        </div>
+      </div>
+
+      <SectionTitle title="Exercício #2" />
+
+      <div className="center">
+        <span className="text">{text}</span>
+        <span className="text">{number}</span>
+
+        <div>
+          <button className="btn" onClick={() => setNumber(number - 1)}> -1</button>
+          <button className="btn" onClick={() => setNumber(number + 1)}> +1</button>
         </div>
       </div>
     </div>
